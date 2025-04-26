953de8752b5b763f469bdabd46e0d09cc4f09596
MarkRBro69 <markbrodovsky@gmail.com>
2025-04-26 15:09:11 +0300

    comments: added sorting and pagination

--- massages/comments/api/v1/views/comments.py
+++ massages/comments/api/v1/views/comments.py
@@ -2,8 +2,12 @@ from rest_framework.viewsets import ModelViewSet
 
 from comments.api.v1.serializers.comments import CommentSerializer
 from comments.models import Comment
+from core.paginators.paginators import BasePaginator
 
 
 class CommentViewSet(ModelViewSet):
-    queryset = Comment.objects.all()
+    queryset = Comment.objects.select_related('user').all()
     serializer_class = CommentSerializer
+    pagination_class = BasePaginator
+    ordering_fields = ['created_at', 'user__username', 'user__email']
+    ordering = ['-created_at']
--- massages/config/settings.py
+++ massages/config/settings.py
@@ -107,4 +107,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'rest_framework.filters.OrderingFilter',
+    ),
 }
