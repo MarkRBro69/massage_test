0129919b93705649e068414eaf201b6a855eb4cf
MarkRBro69 <markbrodovsky@gmail.com>
2025-04-27 14:59:11 +0300

    files config

--- massages/config/urls.py
+++ massages/config/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 
 api_urls = [
@@ -11,4 +13,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api_urls)),
     path('', include('ws.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
